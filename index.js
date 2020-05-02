123ea1f9bfae3fd9b8ddde9ce28b6fbba94319c5
Wolf Man <wolfman199311@gmail.com>
2020-05-02 17:44:50 +0800

    Add progresSQL

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const cool = require('cool-ascii-faces')
 const path = require('path')
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL || 'postgresql://postgres:admin@localhost:5432/test_table',
+//   ssl: process.env.DATABASE_URL ? true : false
+// })
+
 const PORT = process.env.PORT || 5000
 
 express()
@@ -10,6 +22,18 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   showTimes = () => {
